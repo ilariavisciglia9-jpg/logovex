1d362c1594042541ef03d00680dc1b61e4975347
ilariavisciglia9-jpg <ilaria.visciglia9@gmail.com>
2026-05-20 09:02:50 +0200

    server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('trust proxy', 1);
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static('public'));
 // =====================================================
 // REDIRECT www → non-www
